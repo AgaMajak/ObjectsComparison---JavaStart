5a6bf14d6b1eced64f3d5417244e9e4d43e90f51
Agnieszka Majak <agamajak@wp.pl>
2020-04-30 22:28:41 +0200

    added AnimalFactory and createAnimal method

--- src/AnimalFactory.java
+++ src/AnimalFactory.java
@@ -1,2 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AnimalFactory {
+
+    public Animal createAnimal() {
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.US);
+        System.out.print("Podaj gatunek zwierzęcia: ");
+        String species = scan.nextLine();
+        System.out.print("Podaj wagę zwierzęcia (w kilogramach): ");
+        double weight = scan.nextDouble();
+        scan.nextLine();
+        Animal animal = new Animal(species, weight);
+        return animal;
+    }
 }
+
